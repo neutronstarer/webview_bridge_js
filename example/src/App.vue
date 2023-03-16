8464b9bbcc9c3718243485fa040b7dd70575665e
neutronstarer <neutronstarer@gmail.com>
2023-03-16 23:00:52 +0800

    chore: update npc

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <label>{{msg}}</label>
   <button @click="click">下载</button>
 </template>
